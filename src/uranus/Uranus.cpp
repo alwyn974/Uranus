6aec214b7fb56affb5ec697df3625e7b43dd2967
frenetikk <noa.olivette@epitech.eu>
2023-03-03 15:25:54 +0400

    copy test/engine Main.cpp into feature/engine to Uranus.cpp

--- src/uranus/Uranus.cpp
+++ src/uranus/Uranus.cpp
@@ -6,3 +6,21 @@
 */
 
 #include "uranus/Uranus.hpp"
+
+#include "uranus/engine/Engine.hpp"
+
+#include "uranus/game/scene/SceneMain.hpp"
+#include "uranus/game/scene/SceneGame.hpp"
+
+int main()
+{
+    engine::system::gameInit();
+
+    auto &sceneManager = engine::Manager::getSceneManager();
+    sceneManager->addScene(std::make_shared<SceneMain>());
+    sceneManager->addScene(std::make_shared<SceneGame>());
+    sceneManager->changeScene("Main");
+
+    engine::system::gameLoop();
+    return 0;
+}
