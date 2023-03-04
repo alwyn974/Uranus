86719424556d42cd61f6d95cff4afb8cd50800f0
alwyn974 <alwyn974@gmail.com>
2023-03-04 12:35:08 +0400

    🔥 Remove whole game

--- main.cpp
+++ main.cpp
@@ -5,22 +5,10 @@
 ** main.cpp
 */
 
-#include "game/Enemy.hpp"
-#include "game/Player.hpp"
-#include "game/scene/SceneGame.hpp"
-#include "game/scene/SceneMain.hpp"
-#include "uranus/engine/Engine.hpp"
 #include "uranus/Uranus.hpp"
 
 int main()
 {
-    engine::system::gameInit();
 
-    auto &sceneManager = engine::Manager::getSceneManager();
-    sceneManager->addScene(std::make_shared<SceneMain>());
-    sceneManager->addScene(std::make_shared<SceneGame>());
-    sceneManager->changeScene("Main");
-
-    engine::system::gameLoop();
     return 0;
 }
